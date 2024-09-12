32ca6e0f1576154742beab5ad0e31dee63ba74e3
adarshsharma3 <adarshsharma300704@gmail.com>
2024-09-13 01:02:26 +0530

    added infinite loading page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,12 @@
+const {nextui} = require('@nextui-org/theme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/@nextui-org/theme/dist/components/(table|checkbox|spacer).js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui()],
 }
