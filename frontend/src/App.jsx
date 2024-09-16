b510a0017775e95a88c8594fd378b829d78ec583
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2024-09-16 23:24:56 +0530

    haha

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CameraPage from './pages/CameraPage';
-import CapturedImage from './pages/CapturedImage';
-import LandingPage from './pages/Landing'
-import Banner from './pages/Banner';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CameraPage from "./pages/CameraPage";
+import CapturedImage from "./pages/CapturedImage";
+import LandingPage from "./pages/Landing";
+import Banner from "./pages/Banner";
+
 function App() {
-  return (
-    <Router>
-      <Routes>
-      <Route path="/" element={<LandingPage />} />
-        <Route path="/CameraPage" element={<CameraPage />} />
-        <Route path="/display-image" element={<CapturedImage />} />
-        <Route path="/Information" element={<Banner/>} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/CameraPage" element={<CameraPage />} />
+				<Route path="/display-image" element={<CapturedImage />} />
+				<Route path="/Information" element={<Banner />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
